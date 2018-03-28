2179eb6bb85b7192ee16a41dba58ac84982e8b5d
Cuong Nguyen <cuongnmd00335@gmail.com>
2018-03-28 14:05:02 +0700

    add component Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Footer.scss';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>Footer</div>
+        );
+    }
+}
+
+export default Footer;
