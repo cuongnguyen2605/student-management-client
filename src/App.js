b863eb94094009df94a94abc23f351c25c18f890
Cuong Nguyen <cuongnmd00335@gmail.com>
2018-03-28 13:58:07 +0700

    add App page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Login              from './components/Authenticate/Login';
+import Register           from './components/Authenticate/Register';
+import Header             from './components/Header/Header';
+import Navigation         from './components/Navigation/Navigation';
+import HomePage           from './components/HomePage/HomePage';
+import {
+    Grid,
+    Tab
+}                         from 'semantic-ui-react';
+
+const panes = [
+    {menuItem : 'Login', render: () => <Tab.Pane><Login/></Tab.Pane>},
+    {menuItem : 'Register', render: () => <Tab.Pane><Register/></Tab.Pane>}
+];
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="page">
+                <Grid centered>
+                    <Grid.Row>
+                        <Grid.Column width={6}>
+                            <Tab panes={panes}/>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={12}>
+                            <Header/>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={3}>
+                            <Navigation/>
+                        </Grid.Column>
+                        <Grid.Column width={9}>
+                            <HomePage/>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </div>
+        );
+    }
 }
 
 export default App;
